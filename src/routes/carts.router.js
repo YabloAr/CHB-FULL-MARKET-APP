d54d7ef6bd58d94fc23510bb919f9ddb1b4a9e78
YabloAR <hmsalomon86@gmail.com>
2023-10-04 12:54:04 -0300

    Carts doc, done.

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -8,11 +8,17 @@ const router = Router()
 //GET CARTS
 router.get('/', CartsController.getAll)
 
+//CREATE CART
+router.post('/', CartsController.createCart)
+
 //GET BY ID
 router.get('/:cid', CartsController.getCartById)
 
-//CREATE CART
-router.post('/', CartsController.createCart)
+//UPDATE ARRAY OF PRODUCTS IN CART
+router.put('/:cid', CartsController.replaceProducts)
+
+//EMPTY CART
+router.delete('/:cid', CartsController.emptyCart)
 
 //ADD TO CART
 router.post('/:cid/products/:pid', CartsController.addToCart)
@@ -20,14 +26,9 @@ router.post('/:cid/products/:pid', CartsController.addToCart)
 //UPDATE QUANTITY OF PRODUCT IN CART
 router.put('/:cid/products/:pid', CartsController.updateQuantity)
 
-//UPDATE ARRAY OF PRODUCTS IN CART
-router.put('/:cid', CartsController.replaceProducts)
-
 //DELETE PRODUCT FROM CART
 router.delete('/:cid/products/:pid', CartsController.deleteProductFromCart)
 
-//EMPTY CART
-router.delete('/:cid', CartsController.emptyCart)
 
 
 export default router
